835e2f993d3d6b7cdf1b19d5521f807f2c60c403
Mohiuddin Bhuiyan <muhin08t@gmail.com>
2025-08-01 21:26:46 +0600

    implement bottom area

---
+++ lib/screens/recipe_screen.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/cupertino.dart';
+
+class RecipeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: Text('This is the Recipe Screen'));
+  }
+}
